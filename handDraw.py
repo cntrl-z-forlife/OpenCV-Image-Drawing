6b760e80ab1de333d6b819825e40a3cb0fcbcf54
William <80377189+cntrl-z-forlife@users.noreply.github.com>
2022-04-30 08:06:57 -0400

    Improve comments

--- handDraw.py
+++ handDraw.py
@@ -10,14 +10,14 @@ img = cv2.imread(imgChoice)
 cv2.imshow("Original",img)
 cv2.waitKey(0)
 
-#detect edges
+#make black and white to detect edges, blur image
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 gray = cv2.medianBlur(gray, 3) 
 edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 10)
 cv2.imshow("Edges",edges)
 cv2.waitKey(0)
 
-#cartoon
+#reimpose colors and edges
 color = cv2.bilateralFilter(img, 12, 250, 250) 
 final = cv2.bitwise_and(color, color, mask=edges)
 
