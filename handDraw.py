247469f7e50158bf1a9bb58e25bfde24bd09cf18
William <80377189+cntrl-z-forlife@users.noreply.github.com>
2022-04-29 22:51:16 -0400

    Add files via upload

---
+++ handDraw.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+
+#user input                                                                     
+imgChoice = input('Input image title:')
+
+#import image and convert
+img = cv2.imread(imgChoice)
+cv2.imshow("Original",img)
+cv2.waitKey(0)
+
+#detect edges
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+gray = cv2.medianBlur(gray, 3) 
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 10)
+cv2.imshow("Edges",edges)
+cv2.waitKey(0)
+
+#cartoon
+color = cv2.bilateralFilter(img, 12, 250, 250) 
+final = cv2.bitwise_and(color, color, mask=edges)
+
+cv2.imshow("Final", final)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows() 
